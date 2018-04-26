7de0e083f5ce4af9d510c957eb784080e68599d3
Peter Ahrens <ptrahrens@gmail.com>
2018-04-26 12:35:13 -0400

    made executable

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #usage: test.sh EARLEY GRAMMAR CORPUS
-python3 reference.py $2 $3 | sort > tmp1
-python3 $1 $2 $3 | sort > tmp2
+./reference.py $2 $3 | sort > tmp1
+./$1 $2 $3 | sort > tmp2
 diff tmp1 tmp2
 rm tmp1 tmp2
