312ac020854192ab7c312b5bd49a0dea6486fe92
Jack Feser <feser@csail.mit.edu>
2018-05-13 11:19:50 -0500

    Different weak scaling for cgw.

---
+++ cgw_testing/gen-weak-scaling.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp paragraph.corpus weak-scaling/good-1.txt;
+last=weak-scaling/good-1.txt;
+for i in `seq 2 20`; do
+    next="weak-scaling/good-$i.txt";
+    cat paragraph.corpus $last > $next;
+    last=$next
+done;
