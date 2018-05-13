ba2e831fe6d45cbd5b589eced54b07718951f18a
Jack Feser <feser@csail.mit.edu>
2018-05-13 11:29:59 -0500

    Fix cgw weak scaling.

--- cgw_testing/gen-weak-scaling.sh
+++ cgw_testing/gen-weak-scaling.sh
@@ -4,6 +4,6 @@ cp paragraph.corpus weak-scaling/good-1.txt;
 last=weak-scaling/good-1.txt;
 for i in `seq 2 20`; do
     next="weak-scaling/good-$i.txt";
-    cat paragraph.corpus $last > $next;
+    cat paragraph.corpus $last | tr -d "\n" > $next;
     last=$next
 done;
