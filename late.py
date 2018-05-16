ac4a115276844e6aca6b5e8aa1869267e1088352
Peter Ahrens <ptrahrens@gmail.com>
2018-05-16 19:44:17 -0400

    made the late algorithm less confusing

--- late.py
+++ late.py
@@ -1,35 +1,15 @@
 #!/usr/bin/env python3
 
-# MIT License
-
-# Copyright (c) 2018 Peter Ahrens, John Feser, Robin Hui
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-from util import main, State
+from util import main, State, multidict
 from random import randint
 from queue import Queue
 
 def parse(grammar, sentence):
     work = []
     chart = set()
-    completed = set()
+    replies = multidict()
+    requests = multidict()
+    words = set()
 
     def enqueue(s):
       if s not in chart:
@@ -37,25 +17,23 @@ def parse(grammar, sentence):
       chart.add(s)
 
     def predict(s):
-        completed.add(s)
-        #can be avoided if we know which symbols were requested at this location
-        for rhs in grammar[s.next_word()]:
-            enqueue(State(s.next_word(), rhs, s.loc))
-        #can be avoided if we know all the locations where this symbol was parsed originating at this location
-        for loc in set([c.loc for c in completed if c.finished() and c.lhs == s.next_word() and c.origin == s.loc]):
+        if (s.next_word(), s.loc) not in requests:
+            for rhs in grammar[s.next_word()]:
+                enqueue(State(s.next_word(), rhs, s.loc))
+        requests[(s.next_word(), s.loc)].append(s)
+        for loc in replies[(s.next_word(), s.loc)]:
             enqueue(s.incr_word(loc))
 
     def scan(s):
-        completed.add(s)
         if s.loc + 1 <= len(sentence) and sentence[s.loc] == s.next_word():
             enqueue(s.incr_word(s.loc + 1))
 
-    #can be avoided if we know this symbol was already parsed originating and ending at the same origin and location
     def complete(s):
-        completed.add(s)
-        #can be avoided if we know all the rules at this origin that requested this symbol
-        for c in [c for c in completed if (not c.finished()) and c.next_word() == s.lhs and c.loc == s.origin]:
-            enqueue(c.incr_word(s.loc))
+        if (s.lhs, s.origin, s.loc) not in words: #Should add this to words atomically and do this branch on the result.
+            words.add((s.lhs, s.origin, s.loc))
+            replies[(s.lhs, s.origin)].append(s.loc)
+            for r in requests[(s.lhs, s.origin)]:
+                enqueue(r.incr_word(s.loc))
 
     for rhs in grammar['START']:
         enqueue(State('START', rhs, 0))
